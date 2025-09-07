01864309f24b3c6349bdbf5de660040dbeffd36f
Cauanpss <caunp.santos@yahoo.com.br>
2025-09-07 18:37:19 -0300

    initialize commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Services from './pages/Services';
+// import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/sobre" element={<About />} /> */}
+            {/* <Route path="/servicos" element={<Services />} /> */}
+            {/* <Route path="/contato" element={<Contact />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
