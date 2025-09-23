3273bde23c6a7cc882ce055b4584af980d4c5725
unknown <cauanp.santos@yahoo.com.br>
2025-09-23 14:20:03 -0300

    refactor: componentização do switch de idioma para TranslateButton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../components/styles.css";
 import HomeContent from "../components/HomeContent";
+import TranslateButtons from "../components/TranslateButton";
 
 export default function Home() {
     return (
         <div>
+            <TranslateButtons />
             <HomeContent />
         </div>
     );
