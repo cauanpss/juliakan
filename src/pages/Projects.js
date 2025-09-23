ef883c96107766a73033a0362a81b75eab5496b9
unknown <cauanp.santos@yahoo.com.br>
2025-09-23 17:40:28 -0300

    feat: TranslateButtons in Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HoverCard from "../components/HoverCard";
 import importAll from "../util/importAll";
+import TranslateButtons from "../components/TranslateButton";
 
 //importAll de cada card (uma variável para cada card)
 const card1Images = importAll(
@@ -13,35 +14,41 @@ const card1Images = importAll(
 );
 
 const card2Images = importAll(
-    require.context("..assets/images/Projeto_2_NomeDoProjeto"),
+    require.context("../assets/images/Projeto_2_NomeDoProjeto"),
     false,
     /\.(png|jpe?g)$/
 );
 
 const card3Images = importAll(
-    require.context("..assets/images/Projeto_3_NomeDoProjeto"),
+    require.context("../assets/images/Projeto_3_NomeDoProjeto"),
     false,
     /\.(png|jpe?g)$/
 );
 
 const card4Images = importAll(
-    require.context("..assets/images/Projeto_4_NomeDoProjeto"),
+    require.context("../assets/images/Projeto_4_NomeDoProjeto"),
     false,
     /\.(png|jpe?g)$/
 );
 
 //configurações do card
 const hoverCardData = [
-    {images: card1Images, text: "TextoDoProjeto", interval: 1200},
-    {images: card2Images, text: "TextoDoProjeto", interval: 1200},
-    {images: card3Images, text: "TextoDoProjeto", interval: 1200},
-    {images: card4Images, text: "TextoDoProjeto", interval: 1200},
-]
+    { images: card1Images, text: "TextoDoProjeto", interval: 1200 },
+    { images: card2Images, text: "TextoDoProjeto", interval: 1200 },
+    { images: card3Images, text: "TextoDoProjeto", interval: 1200 },
+    { images: card4Images, text: "TextoDoProjeto", interval: 1200 },
+];
 
 export default function Projects() {
     return (
-        <div className= "projects-container">
-            <HoverCard {...hoverCardProps} />
-        </div>
+        <>
+            <TranslateButtons />
+
+            <div className="hoverCard-containers">
+                {hoverCardData.map((props, index) => (
+                    <HoverCard key={index} {...props} />
+                ))}
+            </div>
+        </>
     );
 }
