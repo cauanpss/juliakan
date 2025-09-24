49728563b7e8b51dedf978c8f1d7b45b27131c60
Zokyn <gustavo.casaes@mail.uft.edu.br>
2025-09-24 15:28:43 -0300

    reafc(indexes): convertido export default para array em const e adicionado imports individuais

--- src/assets/images/VisualArts/Horizons/index.js
+++ src/assets/images/VisualArts/Horizons/index.js
@@ -1,3 +1,7 @@
 import Horizons1_png from "./Horizons1.png";
 
-export default [Horizons1_png];
+const projetoHorizons = [Horizons1_png];
+
+export default projetoHorizons;
+
+export {Horizons1_png}
